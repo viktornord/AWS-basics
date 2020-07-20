c788dc86fb69f901c3318714f5b9a5b8dedeeac6
viktornord1 <viktornord1@gmail.com>
2020-07-20 14:02:48 +0300

    update user-data for public

--- final/resources/user_data_public.sh
+++ final/resources/user_data_public.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xe
-mkdir /root/.aws/
-echo $'[default]\nregion = us-east-1' > /root/.aws/config
+mkdir /home/ec2-user/.aws/
+echo $'[default]\nregion = us-east-1' > /home/ec2-user/.aws/config
 aws s3 cp s3://vurbanas-bucket/calc-0.0.1-SNAPSHOT.jar /home/ec2-user
 yum install -y java-1.8.0-openjdk
+java -jar /home/ec2-user/calc-0.0.1-SNAPSHOT.jar
